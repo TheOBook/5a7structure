d9e5616b1c0318718877a4997964ba2c4f499aa5
TheOBook <o.benkreira.h@gmail.com>
2024-05-08 18:58:08 -0400

    second

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold">Hello World!</h1>
+        <p className="mt-4 text-lg">Welcome to your new app!</p>
+      </div>
+    </main>
   );
 }
